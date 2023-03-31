48c73744c25a4f18055dbc2b617909f392c06141
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2023-04-01 01:59:29 +0200

    Create new calander component and finish UI for the TOP view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
 import { AppComponent } from "./app.component";
 import { CalenderComponent } from "./calender/calender.component";
+import { DropDownModule } from "nativescript-drop-down/angular";
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule],
+  imports: [NativeScriptModule, DropDownModule],
   declarations: [AppComponent, CalenderComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
