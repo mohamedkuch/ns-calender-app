9da0d24408bfaa4ac48a9b737fd41bd27a89c7df
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2023-03-31 23:25:42 +0200

    clean project and rm boilerplate code

---
+++ src/app/calender/calender.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "ns-calender",
+  templateUrl: "./calender.component.html",
+})
+export class CalenderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
