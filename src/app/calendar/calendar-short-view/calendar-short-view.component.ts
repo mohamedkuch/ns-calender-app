adf77a69fdcdceddbd093bf5901361d8acbd8f7c
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2023-04-01 03:47:48 +0200

    Update calendar-short-view component logic

--- src/app/calendar/calendar-short-view/calendar-short-view.component.ts
+++ src/app/calendar/calendar-short-view/calendar-short-view.component.ts
@@ -6,7 +6,36 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./calendar-short-view.component.scss"],
 })
 export class CalendarShortViewComponent implements OnInit {
+  days: Array<string> = ["S", "M", "T", "W", "T", "F", "S"];
+  weeks: Array<Array<number>> = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Get current month and year
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+
+    // Get the number of days in the month
+    const numDays = new Date(year, month + 1, 0).getDate();
+
+    // Get the first day of the month
+    const firstDay = new Date(year, month, 1).getDay();
+
+    // Generate an array of all the dates in the month
+    const dates = Array.from({ length: numDays }, (_, i) => i + 1);
+
+    // Add empty cells to the beginning of the array to align the first date with the correct weekday
+    for (let i = 0; i < firstDay; i++) {
+      dates.unshift(null);
+    }
+
+    // Split the array of dates into chunks of 7 (for each week)
+    this.weeks = Array.from({ length: 5 }, (_, i) =>
+      dates.slice(i * 7, (i + 1) * 7)
+    );
+
+    console.log("###", this.weeks);
+  }
 }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { SelectedIndexChangedEventData } from "nativescript-drop-down";
 
 @Component({
   selector: "ns-calendar",
@@ -7,33 +6,7 @@ import { SelectedIndexChangedEventData } from "nativescript-drop-down";
   styleUrls: ["./calendar.component.scss"],
 })
 export class CalendarComponent implements OnInit {
-  // Properties select menu items
-  public selectedPropertyIndex = 0;
-  public propertySelectItems: Array<string> = [
-    "All properties",
-    "2 Zimmer in Stendal",
-    "Test",
-  ];
-
-  // Agents select menu items
-  public selectedAgentIndex = 0;
-  public AgentSelectItems: Array<string> = ["All agents", "Agent 1", "Agent 2"];
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  public onchange(args: SelectedIndexChangedEventData): void {
-    console.log(
-      `Drop Down selected index changed from ${args.oldIndex} to ${args.newIndex}`
-    );
-  }
-
-  public onopen(): void {
-    console.log("Drop Down opened.");
-  }
-
-  public onclose(): void {
-    console.log("Drop Down closed.");
-  }
 }
